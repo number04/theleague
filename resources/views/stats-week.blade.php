a35dcde777d1099c9777030eed88030cd0dca7bd
Matthew Schneider <schn.matthew@gmail.com>
2016-11-17 00:37:42 +1300

    update

--- resources/views/stats-week.blade.php
+++ resources/views/stats-week.blade.php
@@ -4,9 +4,9 @@
 <div class="container">
     <?php
 
-    $week_number = '05';
-    $start_date = '2016-11-07';
-    $end_date = '2016-11-14';
+    $week_number = '06';
+    $start_date = '2016-11-14';
+    $end_date = '2016-11-21';
 
     // teams
 
