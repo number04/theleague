225235231f0a083e2aad6a832891bb67654c699c
Matthew Schneider <schn.matthew@gmail.com>
2017-03-13 21:55:10 +1300

    playoffs

--- resources/views/stats-week.blade.php
+++ resources/views/stats-week.blade.php
@@ -13,21 +13,21 @@
     function teamQuery($start_date, $end_date, $week_number)
     {
         $json = file_get_contents('http://www.nhl.com/stats/rest/individual/team/basic/game/teamsummary?cayenneExp=gameDate%3E=%22'.$start_date.'T11:00:00.000Z%22%20and%20gameDate%3C=%22'.$end_date.'T11:00:00.000Z%22%20and%20gameTypeId=2&factCayenneExp=gamesPlayed%3E=1');
+
         $array = json_decode($json, true);
         $data = $array["data"];
 
         foreach($data as $val) {
 
-            print('UPDATE teams_stats JOIN teams ON teams_stats.player_id = teams.id SET ');
-            print('teams_stats.gp_'.$week_number.' = '.$val['gamesPlayed'].', ');
-            print('teams_stats.w_'.$week_number.' = '.$val['wins'].', ');
-            print('teams_stats.l_'.$week_number.' = '.$val['losses'].', ');
-            print('teams_stats.o_'.$week_number.' = '.$val['otLosses'].', ');
-            print('teams_stats.p_'.$week_number.' = '.$val['points'].', ');
-            print('teams_stats.f_'.$week_number.' = '.$val['goalsFor'].', ');
-            print('teams_stats.a_'.$week_number.' = '.$val['goalsAgainst']);
-            print(' WHERE teams.nhl = "'.$val['teamAbbrev'].'";');
-            echo '<br>';
+            print('UPDATE teams_stats JOIN teams ON teams_stats.player_id = teams.id SET
+                teams_stats.gp_'.$week_number.' = '.$val['gamesPlayed'].',
+                teams_stats.w_'.$week_number.' = '.$val['wins'].',
+                teams_stats.l_'.$week_number.' = '.$val['losses'].',
+                teams_stats.o_'.$week_number.' = '.$val['otLosses'].',
+                teams_stats.p_'.$week_number.' = '.$val['points'].',
+                teams_stats.f_'.$week_number.' = '.$val['goalsFor'].',
+                teams_stats.a_'.$week_number.' = '.$val['goalsAgainst'].'
+                WHERE teams.nhl = "'.$val['teamAbbrev'].'";');
         };
     }
 
