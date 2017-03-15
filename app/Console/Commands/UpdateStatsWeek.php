5b819e195f724390371ba9a37f91526a3bc512b4
Matthew Schneider <schn.matthew@gmail.com>
2017-03-15 20:58:42 +1300

    fixed command name

--- app/Console/Commands/UpdateStatsWeek.php
+++ app/Console/Commands/UpdateStatsWeek.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use DB;
 
-class WeekStats extends Command
+class UpdateStatsWeek extends Command
 {
     /**
      * The name and signature of the console command.
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('inspire')
+        $schedule->command('UpdateStatsWeek')
                  ->dailyAt('08:00');
     }
 
